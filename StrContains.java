831b8d70ca3f29894b0bf2cf209e987570cd2800
JeevaK1996 <jeeva.kcse@ksrce.ac.in>
2016-07-22 15:25:36 +0530

    Create StrContains.java

---
+++ StrContains.java
@@ -0,0 +1,18 @@
+# StrCon
+public class StrContains {
+
+	
+	public static void main(String[] args) {
+	
+		String str="hello";
+		if(str.contains("e")){
+			System.out.println("yes found");
+		}else{
+			System.out.println("not found");
+		}
+		
+		
+
+	}
+
+}
